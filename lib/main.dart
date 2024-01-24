61fcc3b56cffed14b66093484215bcbe2e2d9fb7
Dhrumil <dhrumil@dhrumils-mbp.home>
2024-01-24 18:52:23 -0500

    add name and contact details

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      Myapp()
-  );
+  runApp(Myapp());
 }
 
 class Myapp extends StatelessWidget {
@@ -15,7 +13,11 @@ class Myapp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         appBar: AppBar(
-          title: Center(child: Text("My Card",style: TextStyle(color: Colors.black,fontSize: 20.0),)),
+          title: Center(
+              child: Text(
+            "My Card",
+            style: TextStyle(color: Colors.black, fontSize: 20.0),
+          )),
           backgroundColor: Colors.lightBlue,
         ),
         body: SafeArea(
@@ -27,7 +29,57 @@ class Myapp extends StatelessWidget {
                   radius: 50.0,
                   backgroundImage: AssetImage("images/dhrumil.jpeg"),
                 ),
-              )
+              ),
+              Column(
+                children: [
+                  Text(
+                    "Dhrumil Desai",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30.0,
+                        fontFamily: "Pacifico"),
+                  ),
+                  Text(
+                    "Flutter Developer",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(15.0),
+                        margin: EdgeInsets.symmetric(vertical: 15.0,horizontal: 15.0),
+                        color: Colors.white,
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.phone,size: 20.0,color: Colors.black,),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text("+1 4168574427",style: TextStyle(color: Colors.black,fontSize: 20.0),)
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(15.0),
+                        margin: EdgeInsets.symmetric(vertical: 15.0,horizontal: 15.0),
+                        color: Colors.white,
+                        child: Row(
+                          children: <Widget>[
+                            Icon(Icons.email,size: 20.0,color: Colors.black,),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            Text("dddhrumil21@gmail.com",style: TextStyle(color: Colors.black,fontSize: 20.0),)
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ],
           ),
         ),
